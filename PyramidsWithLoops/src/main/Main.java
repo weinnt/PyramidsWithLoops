3d14e5001510f6aca4090af63259208983ebf856
Noah <Noah@192.168.0.12>
2019-09-25 21:10:35 -0400

    Final Finished program

--- PyramidsWithLoops/src/main/Main.java
+++ PyramidsWithLoops/src/main/Main.java
@@ -1,9 +1,9 @@
 /*
- * Bill Nicholson
- * nicholdw@ucmail.uc.edu
- * Printing pyramids using loops. It kind-of works. 
- * What I really want is
-        1
+ * Bill Nicholson////*****Noah Wein
+ * nicholdw@ucmail.uc.edu////*****weinnt@mail.uc.edu											
+ * Printing pyramids using loops. It kind-of works.////***** 
+ * What I really want is////*****
+		1
        222
       33333
      4444444
@@ -14,18 +14,29 @@ package main;
 public class Main {
 
 	public static void main(String[] args) {
-		int i = 0, lines = 5;
-		while (i < lines) {i++;for(int j = 0; j < i+1; j++) { System.out.print(j);}System.out.println("");
-}
-		
-		/*for (int i = 0; i < lines; i++) {
-			for (int j = 0; j < i+1; j++) {
-				System.out.print(j);
+		/*int lines = 5, rows = 1;
+		 
+		for (int i=0; i<=lines; i++)System.out.print(" "+rows);for (int j=1; j<rows; j++){System.out.print(rows);}System.out.println("");rows++;}*/
+		int lines = 5;
+		for (int i = 1; i <= lines; i++) {
+			for (int j = lines+4; j>i; j--) {
+				System.out.print(" ");
 			}
-			System.out.println("");
-			*/
+			for (int k = 1; k <=i; k++) {
+				System.out.print(i + "");
+			}
+			for (int k = i; k >=2; k--) {
+				System.out.print(i+ "");
+			}
+			System.out.println();
+			}
+			
+			}}
+			
 		
 			
-		}
-	}
+		
+
+	
+	
 
